cc91fbef76e32c4f4c5ab1aa9078c77407ca26e8
Will Alexander <willwalexander@icloud.com>
2018-03-11 17:51:03 -0700

    Improve usage message

--- README.md
+++ README.md
@@ -11,14 +11,14 @@ Building
 Usage
 -----
 
-    imgscheme [-[n] color] [path]
+    usage: imgscheme [-n color]... file
 
-imgscheme generates a terminal color scheme from the image at the named path. It
-attempts to make the generated colors correspond to prominent colors in the
-image, and also close to the corresponding colors in a base color scheme. The
-colors in the base scheme can be set using numeric flags corresponding to the 16
-ANSI colors (0-15) as hex triplets /[0-9a-f]{6}/ The generated colors will be
-output in order as hex triplets.
+imgscheme generates a terminal color scheme from an image file. It attempts to
+make the generated colors correspond to prominent colors in the image, and also
+close to the corresponding colors in a base color scheme. The colors in the base
+scheme can be set using numeric flags corresponding to the 16 ANSI colors (0-15)
+as hex triplets /[0-9a-f]{6}/ The generated colors will be output in order as
+hex triplets.
 
 The output of imgscheme can be rendered on
 [this page](https://wwalexander.github.io/imgscheme/).
--- imgscheme.go
+++ imgscheme.go
@@ -138,14 +138,14 @@ var SchemeVGA = color.Palette{
 	RGB{0xff, 0xff, 0xff},
 }
 
-const usage = `usage: imgscheme [-[n] color] [path]
+const usage = `usage: imgscheme [-n color]... file
 
-imgscheme generates a terminal color scheme from the image at the named path. It
-attempts to make the generated colors correspond to prominent colors in the
-image, and also close to the corresponding colors in a base color scheme. The
-colors in the base scheme can be set using numeric flags corresponding to the 16
-ANSI colors (0-15) as hex triplets /[0-9a-f]{6}/ The generated colors will be
-output in order as hex triplets.`
+imgscheme generates a terminal color scheme from an image file. It attempts to
+make the generated colors correspond to prominent colors in the image, and also
+close to the corresponding colors in a base color scheme. The colors in the base
+scheme can be set using numeric flags corresponding to the 16 ANSI colors (0-15)
+as hex triplets /[0-9a-f]{6}/ The generated colors will be output in order as
+hex triplets.`
 
 func main() {
 	colors := []*string{
